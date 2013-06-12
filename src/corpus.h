1bea5eec789d62f928522b17f5c2f30f6540ad46
Monfee <eric.tang2005@gmail.com>
2013-06-12 12:12:37 +1000

    added license declaration for each source file

--- src/corpus.h
+++ src/corpus.h
@@ -1,4 +1,20 @@
-/*
+/******************************************************************************
+ * This file is part of the XLINK - The Link Discovery Toolkit.
+ * (c) Copyright 2013 Queensland University of Technology
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * BSD 3-cause LICENSE appearing in the file BSD.txt included in the
+ * packaging of this file.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *******************************************************************************
+ *
+ * @author				Eric Tang
+ *
+ *******************************************************************************/
+ /*
  * corpus.h
  *
  *  Created on: Sep 16, 2009
