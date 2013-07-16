32fada3323aff5084f8858aed37d14b11f148829
Monfee <eric.tang2005@gmail.com>
2013-07-16 11:13:06 +1000

    finally, get the chrome plugin working a bit

--- plugins/chrome/wikifyme.js
+++ plugins/chrome/wikifyme.js
@@ -1,3 +1,7 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(null, {file: "events.js"});
-});
+//if (tab.url.indexOf("chrome-devtools://") == -1) {
+	chrome.browserAction.onClicked.addListener(
+			function(tab) {
+				chrome.tabs.executeScript(null, {file: "events.js"});
+			}
+	);
+//}
